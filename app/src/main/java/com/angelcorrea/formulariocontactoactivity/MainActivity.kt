90f44aefa6ab70924d88608d3b329a5a291c0070
AngelUPT <angel_1322134008@uptecamac.edu.mx>
2025-01-26 13:26:43 -0600

    adding action

--- app/src/main/java/com/angelcorrea/formulariocontactoactivity/MainActivity.kt
+++ app/src/main/java/com/angelcorrea/formulariocontactoactivity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.angelcorrea.formulariocontactoactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,28 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val nombreEditText = findViewById<EditText>(R.id.editTextText)
+        val telefonoEditText = findViewById<EditText>(R.id.editTextText2)
+        val mensajeEditText = findViewById<EditText>(R.id.editTextText3)
+        val enviarButton = findViewById<Button>(R.id.button)
+        enviarButton.setOnClickListener {
+            val nombre = nombreEditText.text.toString()
+            val telefono = telefonoEditText.text.toString()
+            val mensaje = mensajeEditText.text.toString()
+
+            if (nombre.isEmpty() || telefono.isEmpty() || mensaje.isEmpty()) {
+                Toast.makeText(this, "Por favor, llena todos los campos.", Toast.LENGTH_SHORT).show()
+            } else {
+                val datosUsuario = """
+                    Nombre: $nombre
+                    Teléfono: $telefono
+                    Mensaje: $mensaje
+                """.trimIndent()
+                Toast.makeText(this, datosUsuario, Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
